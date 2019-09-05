bfda3bfcaaddb5f0f745331bd98ecb0ac8a5665a
tester <13697485262@139.com>
2019-09-05 21:02:36 +0800

    体检course list post 做测试

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -2,10 +2,13 @@
 from django.conf.urls import url
 from apps.api.views_demo import *
 from apps.api.view_index import *
+from apps.api.view_courses import *
 
 urlpatterns = [
     url(r'^test_demo/',test_demo,name='test_demo'),
     url(r'^index/',index_page,name='index'),
+    url(r'^course/',course_list,name='course_list'),
+
     url(r'^$',test_null,name='test_null'),
 
 
---
+++ apps/api/view_courses.py
@@ -0,0 +1,66 @@
+from django.http import JsonResponse
+from django.core.exceptions  import ValidationError
+
+from courses.models import Course
+from organization.models import CourseOrg
+from users.models import Banner
+from pure_pagination import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import Q
+
+def course_list(request):
+    '''课程接口'''
+    '''
+    参数解析：
+    
+    搜索关键字<keywords>：搜索字段
+    排序方式<sort>：   add_time（默认）、students、click_nums
+    
+    返回：
+    
+    '''
+    try:
+        all_courses = Course.objects.all().order_by('-add_time')
+        hot_courses = Course.objects.all().order_by('-click_nums')[:3]
+
+        #课程搜索
+        search_keywords = request.GET.get('keywords','')
+        if search_keywords:
+            all_courses = all_courses.filter(Q(name__icontains=search_keywords)|
+                                             Q(desc__icontains=search_keywords)|
+                                             Q(detail__icontains=search_keywords))
+
+        # 课程排序
+        sort = request.GET.get('sort',"")
+
+        if sort:
+            if sort == "students":
+                all_courses = all_courses.order_by("-students")
+            elif sort == "hot":
+                all_courses = all_courses.order_by("-click_nums")
+
+        try:
+            page = request.GET.get('page',1)
+        except PageNotAnInteger:
+            page = 1
+
+        p = Paginator(all_courses, 6, request=request)
+        courses = p.page(page)
+
+        return JsonResponse(
+            {"status":200,
+             'data':
+                 {
+                     'all_courses':courses,
+                     'hot_courses':hot_courses,
+                 }
+             }
+            , json_dumps_params={'ensure_ascii':False})
+
+    except BaseException:
+        pass
+
+    return JsonResponse(
+        {"status": 500,
+         'msg': "参数错误"
+         }
+        , json_dumps_params={'ensure_ascii':False})
