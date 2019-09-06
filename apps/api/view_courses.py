e9d1fd80c810f73ddd07efa88c63c2acd5369fc6
tester <13697485262@139.com>
2019-09-06 10:49:51 +0800

    post接口修改

--- apps/api/view_courses.py
+++ apps/api/view_courses.py
@@ -18,49 +18,59 @@ def course_list(request):
     返回：
     
     '''
-    try:
-        all_courses = Course.objects.all().order_by('-add_time')
-        hot_courses = Course.objects.all().order_by('-click_nums')[:3]
-
-        #课程搜索
-        search_keywords = request.GET.get('keywords','')
-        if search_keywords:
-            all_courses = all_courses.filter(Q(name__icontains=search_keywords)|
-                                             Q(desc__icontains=search_keywords)|
-                                             Q(detail__icontains=search_keywords))
-
-        # 课程排序
-        sort = request.GET.get('sort',"")
-
-        if sort:
-            if sort == "students":
-                all_courses = all_courses.order_by("-students")
-            elif sort == "hot":
-                all_courses = all_courses.order_by("-click_nums")
+    if request.method == 'POST':
 
         try:
-            page = request.GET.get('page',1)
-        except PageNotAnInteger:
-            page = 1
 
-        p = Paginator(all_courses, 6, request=request)
-        courses = p.page(page)
+            if not request.POST:
+                return JsonResponse(
+                    {"status": 500,
+                     'msg': "参数错误"
+                     }
+                    , json_dumps_params={'ensure_ascii':False})
+
+            all_courses = Course.objects.all().order_by('-add_time')
+            hot_courses = Course.objects.all().order_by('-click_nums')[:3]
+
+            #课程搜索
+            search_keywords = request.POST.get('keywords','')
+            if search_keywords:
+                all_courses = all_courses.filter(Q(name__icontains=search_keywords)|
+                                                 Q(desc__icontains=search_keywords)|
+                                                 Q(detail__icontains=search_keywords))
+
+            # 课程排序
+            sort = request.POST.get('sort',"")
+
+            if sort:
+                if sort == "students":
+                    all_courses = all_courses.order_by("-students")
+                elif sort == "hot":
+                    all_courses = all_courses.order_by("-click_nums")
+
+            try:
+                page = request.POST.get('page',1)
+            except PageNotAnInteger:
+                page = 1
+
+            p = Paginator(all_courses, 6, request=request)
+            courses = p.page(page)
+
+            return JsonResponse(
+                {"status":200,
+                 'data':
+                     {
+                         'all_courses':courses,
+                         'hot_courses':hot_courses,
+                     }
+                 }
+                , json_dumps_params={'ensure_ascii':False})
+
+        except BaseException:
+            pass
 
         return JsonResponse(
-            {"status":200,
-             'data':
-                 {
-                     'all_courses':courses,
-                     'hot_courses':hot_courses,
-                 }
+            {"status": 500,
+             'msg': "参数错误"
              }
             , json_dumps_params={'ensure_ascii':False})
-
-    except BaseException:
-        pass
-
-    return JsonResponse(
-        {"status": 500,
-         'msg': "参数错误"
-         }
-        , json_dumps_params={'ensure_ascii':False})
