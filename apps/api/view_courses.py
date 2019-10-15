6aaa609ab14b3f00b19b4d8e3da16dc8c4474697
tester <13697485262@139.com>
2019-10-15 21:11:59 +0800

    添加部分借口

--- apps/api/view_courses.py
+++ apps/api/view_courses.py
@@ -10,6 +10,64 @@ from operation.models import UserFavorite, CourseComments, UserCourse
 from users.forms import LoginForm
 
 
+
+def course_list(courses):
+    '''将course转为list'''
+    course_list=[]
+    for c in courses:
+        course_list.append(
+            {
+                "org_name" : c.course_org.name,
+                'teacher' : c.teacher.name,
+                'name' : c.name,
+                'desc' : c.desc,
+                'degree' : c.detail,
+                'learn_times' : c.learn_times,
+                'students': c.students,
+                'fav_nums' : c.fav_nums,
+                'image' : str(c.image),
+                'click_nums' : c.click_nums,
+                'category' : c.category,
+                'is_banner' : c.is_banner,
+                'youneed_know' : c.youneed_know,
+                'teacher_tell' : c.teacher_tell,
+                'tag' : c.tag,
+                'add_time' : c.add_time
+            }
+        )
+    return course_list
+
+def course_Resourse_list(courses_rescourse):
+    courses_rescourse_list=[]
+    for c in courses_rescourse:
+        courses_rescourse_list.append(
+            {
+                "course" : c.course.course_org.name,
+                'name' : c.name,
+                'download' : str(c.download),
+                'add_time' : c.add_time
+            }
+        )
+
+    return courses_rescourse_list
+
+
+def course_Comments_list(comments):
+
+    course_Comments_list=[]
+    for c in comments:
+        course_Comments_list.append(
+            {
+                "user" : c.user.nick_name,
+                "course" : c.course.course_org.name,
+                'comments' : c.comments,
+                'add_time' : c.add_time
+            }
+        )
+    return course_Comments_list
+
+
+
 class CourseListAPIView(View):
     '''课程接口'''
     '''
@@ -19,7 +77,6 @@ class CourseListAPIView(View):
     排序方式<sort>：   add_time（默认）、students、click_nums
     
     返回：
-    
     '''
     def post(self, request):
         try:
@@ -58,61 +115,19 @@ class CourseListAPIView(View):
             p = Paginator(all_courses, 6, request=request)
             courses = p.page(page)
 
-
+            courses = course_list(courses)
             #QuerySet 需要转为 字典 + list
-            course_list1=[]
-            for c in all_courses:
-                course_list1.append(
-                    {
-                        "org_name" : c.course_org.name,
-                        'teacher' : c.teacher.name,
-                        'name' : c.name,
-                        'desc' : c.desc,
-                        'degree' : c.detail,
-                        'learn_times' : c.learn_times,
-                        'students' : c.students,
-                        'fav_nums' : c.fav_nums,
-                        'image' : str(c.image),
-                        'click_nums' : c.click_nums,
-                        'category' : c.category,
-                        'is_banner' : c.is_banner,
-                        'youneed_know' : c.youneed_know,
-                        'teacher_tell' : c.teacher_tell,
-                        'tag' : c.tag,
-                        'add_time' : c.add_time
-                    }
-                )
+            all_courses = course_list(all_courses)
 
-
-            course_list2=[]
-            for c in hot_courses:
-                course_list2.append(
-                    {
-                        "org_name" : c.course_org.name,
-                        'teacher' : c.teacher.name,
-                        'name' : c.name,
-                        'desc' : c.desc,
-                        'degree' : c.detail,
-                        'learn_times' : c.learn_times,
-                        'students' : c.students,
-                        'fav_nums' : c.fav_nums,
-                        'image' : str(c.image),
-                        'click_nums' : c.click_nums,
-                        'category' : c.category,
-                        'is_banner' : c.is_banner,
-                        'youneed_know' : c.youneed_know,
-                        'teacher_tell' : c.teacher_tell,
-                        'tag' : c.tag,
-                        'add_time' : c.add_time
-                    }
-                )
+            hot_courses = course_list(hot_courses)
 
             return JsonResponse(
                 {"status":200,
                  'data':
                      {
-                         'all_courses':course_list1,
-                         'hot_courses':course_list2,
+                         'courses':courses,
+                         'all_courses':all_courses,
+                         'hot_courses':hot_courses,
                      }
                  }
                 , json_dumps_params={'ensure_ascii':False})
@@ -126,7 +141,7 @@ class CourseListAPIView(View):
                 , json_dumps_params={'ensure_ascii':False})
 
 class CourseDetailAPIView(View):
-
+    ''''''
     def post(self, request):
         '''
         课程详情页
@@ -135,134 +150,89 @@ class CourseDetailAPIView(View):
         :return:
         将用户get接口，改为post接口仅仅用于接口测试
         '''
-        try:
-            course_id = request.POST.get("course_id","")
 
-            #user = authenticate(username = user_name ,password = pass_word)
+        course_id = request.POST.get("course_id","")
 
-            course = Course.objects.get(id=int(course_id))
+        course = Course.objects.get(id=int(course_id))
 
-            course.click_nums +=1
-            course.save()
+        course.click_nums +=1
+        course.save()
 
-            courses=[]
-            courses.append(
-                {
-                    "org_name" : course.course_org.name,
-                    'teacher' : course.teacher.name,
-                    'name' : course.name,
-                    'desc' : course.desc,
-                    'degree' : course.detail,
-                    'learn_times' : course.learn_times,
-                    'students' : course.students,
-                    'fav_nums' : course.fav_nums,
-                    'image' : str(course.image),
-                    'click_nums' : course.click_nums,
-                    'category' : course.category,
-                    'is_banner' : course.is_banner,
-                    'youneed_know' : course.youneed_know,
-                    'teacher_tell' : course.teacher_tell,
-                    'tag' : course.tag,
-                    'add_time' : course.add_time
-                }
-            )
+        courses=[]
+        courses.append(
+            {
+                "org_name" : course.course_org.name,
+                'teacher' : course.teacher.name,
+                'name' : course.name,
+                'desc' : course.desc,
+                'degree' : course.detail,
+                'learn_times' : course.learn_times,
+                'students' : course.students,
+                'fav_nums' : course.fav_nums,
+                'image' : str(course.image),
+                'click_nums' : course.click_nums,
+                'category' : course.category,
+                'is_banner' : course.is_banner,
+                'youneed_know' : course.youneed_know,
+                'teacher_tell' : course.teacher_tell,
+                'tag' : course.tag,
+                'add_time' : course.add_time
+            }
+        )
 
-            has_fav_org = False
-            has_fav_course = False
+        has_fav_org = False
+        has_fav_course = False
 
-            # login_form = LoginForm(request.POST)
-            # if login_form.is_valid():
-            user_name = request.POST.get("username","")
-            pass_word = request.POST.get("password","")
+        user_name = request.POST.get("username","")
+        pass_word = request.POST.get("password","")
 
-            print("user_name = %s, pass_word = %s" %(user_name,pass_word))
-            user = authenticate(username = user_name ,password = pass_word)
+        print("user_name = %s, pass_word = %s" %(user_name,pass_word))
+        user = authenticate(username = user_name ,password = pass_word)
 
+        if user.is_authenticated():
+            if UserFavorite.objects.filter(user=user,
+                                           fav_id=course.id, fav_type=1):
+                has_fav_course = True
+            if UserFavorite.objects.filter(user=user,
+                                           fav_id=course.course_org.id, fav_type=2):
+                has_fav_org = True
 
-            if user.is_authenticated():
-                if UserFavorite.objects.filter(user=user,
-                                               fav_id=course.id, fav_type=1):
-                    has_fav_course = True
-                if UserFavorite.objects.filter(user=user,
-                                               fav_id=course.course_org.id, fav_type=2):
-                    has_fav_org = True
+        tag = course.tag
+        if tag:
+            relate_courses = Course.objects.filter(~Q(id=course.id),tag=tag)[:1]
 
+            relate_courses = course_list(relate_courses)
 
-            tag = course.tag
-            if tag:
-                relate_courses = Course.objects.filter(~Q(id=course.id),tag=tag)[:1]
+        else:
+            relate_courses = []
 
-                relate_course = []
-                for c in relate_courses:
-                    relate_course.append(
-                        {
-                            "org_name" : c.course_org.name,
-                            'teacher' : c.teacher.name,
-                            'name' : c.name,
-                            'desc' : c.desc,
-                            'degree' : c.detail,
-                            'learn_times' : c.learn_times,
-                            'students' : c.students,
-                            'fav_nums' : c.fav_nums,
-                            'image' : str(c.image),
-                            'click_nums' : c.click_nums,
-                            'category' : c.category,
-                            'is_banner' : c.is_banner,
-                            'youneed_know' : c.youneed_know,
-                            'teacher_tell' : c.teacher_tell,
-                            'tag' : c.tag,
-                            'add_time' : c.add_time
-                        }
-                    )
-                relate_courses = relate_course
-
-
-            else:
-                relate_courses = []
-
-            return JsonResponse(
-                {"status":200,
-                 'data':
-                     {
-                         'course':courses,
-                         'relate_courses':relate_courses,
-                         'has_fav_course':has_fav_course,
-                         'has_fav_org':has_fav_org,
-                     }
+        return JsonResponse(
+            {"status":200,
+             'data':
+                 {
+                     'course':courses,
+                     'relate_courses':relate_courses,
+                     'has_fav_course':has_fav_course,
+                     'has_fav_org':has_fav_org,
                  }
-                , json_dumps_params={'ensure_ascii':False})
-        except BaseException as be:
-            return JsonResponse(
-                {"status":500,
-                 'data':
-                     {
-                         'msg':be,
+             }
+            , json_dumps_params={'ensure_ascii':False})
 
-                     }
-                 }
-                , json_dumps_params={'ensure_ascii':False})
 
 class CourseInfoAPIView(View):
     '''课程章节信息'''
     def post(self, request):
-
         course_id = request.POST.get("course_id","")
         course = Course.objects.get(id=int(course_id))
 
         course.students +=1
         course.save()
 
-        login_form = LoginForm(request.POST)
-        user = None
-        if login_form.is_valid():
+        user_name = request.POST.get("username","")
+        pass_word = request.POST.get("password","")
 
-            user_name = request.POST.get("username","")
-            pass_word = request.POST.get("password","")
-
-            print("user_name = %s, pass_word = %s" %(user_name,pass_word))
-            user = authenticate(username = user_name ,password = pass_word)
-
-
+        print("user_name = %s, pass_word = %s" %(user_name,pass_word))
+        user = authenticate(username = user_name ,password = pass_word)
 
         # 查询用户是否已经关联了该课程
         user_cs = UserCourse.objects.filter(user=user, course=course)
@@ -270,8 +240,6 @@ class CourseInfoAPIView(View):
         if not user_cs:
             user_course = UserCourse(user=user, course=course)
             user_course.save()
-
-
 
         user_courses = UserCourse.objects.filter(course=course)
 
@@ -284,12 +252,40 @@ class CourseInfoAPIView(View):
         relate_courses = Course.objects.filter(id__in=course_ids).order_by('-click_nums')[:5]
 
         all_resources = CourseResourse.objects.filter(course=course)
+        # 转换为courselist
+
+        courses=[]
+        courses.append(
+            {
+                "org_name" : course.course_org.name,
+                'teacher' : course.teacher.name,
+                'name' : course.name,
+                'desc' : course.desc,
+                'degree' : course.detail,
+                'learn_times' : course.learn_times,
+                'students' : course.students,
+                'fav_nums' : course.fav_nums,
+                'image' : str(course.image),
+                'click_nums' : course.click_nums,
+                'category' : course.category,
+                'is_banner' : course.is_banner,
+                'youneed_know' : course.youneed_know,
+                'teacher_tell' : course.teacher_tell,
+                'tag' : course.tag,
+                'add_time' : course.add_time
+            }
+        )
+
+
+
+        all_resources = course_Resourse_list(all_resources)
+        relate_courses = course_list(relate_courses)
 
         return JsonResponse(
                 {"status":200,
                  'data':
                      {
-                         'course':course,
+                         'course': courses,
                          'all_resources':all_resources,
                          'relate_courses':relate_courses,
                      }
@@ -304,19 +300,13 @@ class CommentAPIView(View):
 
         course = Course.objects.get(id=int(course_id))
 
-        login_form = LoginForm(request.POST)
-        user = None
-        if login_form.is_valid():
+        user_name = request.POST.get("username","")
+        pass_word = request.POST.get("password","")
 
-            user_name = request.POST.get("username","")
-            pass_word = request.POST.get("password","")
-
-            print("user_name = %s, pass_word = %s" %(user_name,pass_word))
-            user = authenticate(username = user_name ,password = pass_word)
-
+        print("user_name = %s, pass_word = %s" %(user_name,pass_word))
+        user = authenticate(username = user_name ,password = pass_word)
 
         all_comments = CourseComments.objects.filter(user=user,course=course)
-
 
         user_courses = UserCourse.objects.filter(course=course)
 
@@ -330,12 +320,39 @@ class CommentAPIView(View):
 
         all_resources = CourseResourse.objects.filter(course=course)
 
+        courses=[]
+        courses.append(
+            {
+                "org_name" : course.course_org.name,
+                'teacher' : course.teacher.name,
+                'name' : course.name,
+                'desc' : course.desc,
+                'degree' : course.detail,
+                'learn_times' : course.learn_times,
+                'students' : course.students,
+                'fav_nums' : course.fav_nums,
+                'image' : str(course.image),
+                'click_nums' : course.click_nums,
+                'category' : course.category,
+                'is_banner' : course.is_banner,
+                'youneed_know' : course.youneed_know,
+                'teacher_tell' : course.teacher_tell,
+                'tag' : course.tag,
+                'add_time' : course.add_time
+            }
+        )
+
+        relate_courses = course_list(relate_courses)
+
+        all_resources = course_Resourse_list(all_resources)
+
+        all_comments = course_Comments_list(all_comments)
 
         return JsonResponse(
                 {"status":200,
                  'data':
                      {
-                         'course':course,
+                         'course':courses,
                          'all_resources':all_resources,
                          'all_comments':all_comments,
                          'relate_courses':relate_courses,
@@ -350,16 +367,11 @@ class AddComentsAPIView(View):
     '''用户评论'''
     def post(self, request):
 
-        login_form = LoginForm(request.POST)
-        user = None
-        if login_form.is_valid():
+        user_name = request.POST.get("username","")
+        pass_word = request.POST.get("password","")
 
-            user_name = request.POST.get("username","")
-            pass_word = request.POST.get("password","")
-
-            print("user_name = %s, pass_word = %s" %(user_name,pass_word))
-            user = authenticate(username = user_name ,password = pass_word)
-
+        print("user_name = %s, pass_word = %s" %(user_name,pass_word))
+        user = authenticate(username = user_name ,password = pass_word)
 
         if user.is_authenticated():
             '''判断用户登录状态'''
@@ -372,7 +384,6 @@ class AddComentsAPIView(View):
                      }
                     , json_dumps_params={'ensure_ascii':False})
 
-
         course_id = request.POST.get('course_id',0)
         comments = request.POST.get('comments','')
 
@@ -381,7 +392,7 @@ class AddComentsAPIView(View):
             course = Course.objects.get(id=course_id)
             course_comments.course = course
             course_comments.comments = comments
-            course_comments.user = request.user
+            course_comments.user = user
             course_comments.save()
             return JsonResponse(
                 {"status":200,
@@ -398,6 +409,7 @@ class AddComentsAPIView(View):
                          "msg":"添加失败"
                      }
                  }, json_dumps_params={'ensure_ascii':False})
+
 
 class VideoPlayAPIView(View):
     '''视频播放页面'''
